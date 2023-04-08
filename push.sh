8924bd2da1314c92f65649efe08ba94c2afb0563
Bakarseck <seck.bakar@ugb.edu.sn>
2023-04-07 19:15:37 -0700

    Push

--- push.sh
+++ push.sh
@@ -1,16 +1,4 @@
-#!/bin/bash
-
-# Récupérer le message de commit en ligne de commande
 commit_message="$1"
-
-# Mettre à jour le dépôt local
-git pull
-
-# Ajouter les fichiers modifiés à la zone de staging
 git add .
-
-# Valider les changements avec le message de commit spécifié
 git commit -m "$commit_message"
-
-# Pousser les modifications vers le dépôt distant
-git push origin master
+git push
