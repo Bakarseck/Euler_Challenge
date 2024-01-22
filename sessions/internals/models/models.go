dc8c6813b7c58f9eeedb84917d1689a2a4b70bb2
bakseck <seck.bakar@ugb.edu.sn>
2024-01-22 16:41:06 +0000

    createUser: gorm

--- sessions/internals/models/models.go
+++ sessions/internals/models/models.go
@@ -1,1 +1,14 @@
 package models
+
+import (
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Username  string `gorm:"unique"`
+	FirstName string
+	LastName  string
+	Email     string `gorm:"unique"`
+	Password  string
+}
--- sessions/internals/utils/utils.go
+++ sessions/internals/utils/utils.go
@@ -1,1 +1,20 @@
 package utils
+
+import (
+	"sessions/internals/models"
+
+	"gorm.io/gorm"
+)
+
+func CreateUser(db *gorm.DB, username, firstName, lastName, email, password string) error {
+
+	user := models.User{
+		Username:  username,
+		FirstName: firstName,
+		LastName:  lastName,
+		Email:     email,
+		Password:  password,
+	}
+
+	return db.Create(&user).Error
+}
